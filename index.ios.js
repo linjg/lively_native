a2d84f7d41356da515d22cd46cc6bf42a1e5cc05
linjg <328695058@qq.com>
2016-04-19 23:15:50 +0800

    no message

---
+++ index.ios.js
@@ -0,0 +1,44 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ */
+
+import React, {
+  AppRegistry,
+  Component,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+var TabBarCom = require('./ios_component/tab_bar_ios.js')
+
+class lively_native extends Component {
+  render() {
+    return (
+      <TabBarCom></TabBarCom>
+      
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+AppRegistry.registerComponent('lively_native', () => lively_native);
