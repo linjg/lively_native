5bc21003c43acf4b532934470abb265fd26ac9c3
linjg <328695058@qq.com>
2016-04-22 03:04:52 +0800

    no message

---
+++ css/style.js
@@ -0,0 +1,1 @@
+module.exports = require('react-native').StyleSheet.create({"wrap":{"flex":1},"inner_view":{"flex":1,"marginTop":10,"marginBottom":10,"marginRight":10,"marginLeft":10},"text":{"flex":1}});
--- ios_component/home.js
+++ ios_component/home.js
@@ -1,62 +1,122 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-var React=require('react-native')
-var{
-  
-  StyleSheet,
-  Text,
+'use strict';
+
+var React = require('react');
+var ReactNative = require('react-native');
+
+var Style=require('../css/style.js')
+
+var Dimensions =require('Dimensions');
+var {
   Image,
- View,
-} = React;
+  ListView,
+  TouchableHighlight,
+  StyleSheet,
+  RecyclerViewBackedScrollView,
+  Text,
+  View,
+} = ReactNative;
 
+// var UIExplorerPage = require('./UIExplorerPage');
 
+var Home= React.createClass({
+  statics: {
+    title: '<ListView>',
+    description: 'Performant, scrollable list of data.'
+  },
 
-
-
-var  Home= React.createClass({
   getInitialState: function() {
-      return {
-          selectedTab: 'main'
-      };
+    var ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+    return {
+      dataSource: ds.cloneWithRows(this._genRows({})),
+    };
   },
-  render:function() {
-    return (
-      <View style={Style.container}>
-        <View style={Style.Image_outer}>
-           <View style={{height:20,backgroundColor:'#333333'}}></View>
-        </View>
-        <View style={Style.Image_outer}>
-          <View style={{height:20,backgroundColor:'#333333'}}></View>
-        </View>
-      </View>
-    );
-  }
-})
+get_screen_width:function(){
 
-var  Style = React.StyleSheet.create({
-  container: {
-    flexDirection:'row',
+return Dimensions.get('window').width;
+},
+  _pressData: ({}: {[key: number]: boolean}),
+
+  componentWillMount: function() {
+    this._pressData = {};
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+
+  render: function() {
+    return (
+
+        <ListView
+          dataSource={this.state.dataSource}
+          renderRow={this._renderRow}
+          renderScrollComponent={props => <RecyclerViewBackedScrollView {...props} />}
+          renderSeparator={(sectionID, rowID) => <View key={`${sectionID}-${rowID}`} style={Style.separator} />}
+        />
+     
+    );
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+
+  _renderRow: function(rowData: string, sectionID: number, rowID: number) {
+    var rowHash = Math.abs(hashCode(rowData));
+    var imgSource = THUMB_URLS[rowHash % THUMB_URLS.length];
+    return (
+      <TouchableHighlight onPress={() => this._pressRow(rowID)}>
+        <View>
+
+            <View style={Style.title}>
+              <View style={Style.avatar_view}>
+                 <Text>qweqewqew</Text>
+              </View>
+            </View>
+
+            <Image style={[Style.thumb, {width:this.get_screen_width()}]} source={imgSource} />
+
+            <Text style={Style.text}>
+              {rowData + ' - ' + LOREM_IPSUM.substr(0, rowHash % 301 + 10)}
+            </Text>
+        </View>
+      </TouchableHighlight>
+    );
   },
-  Image_s:{
-    
+
+  _genRows: function(pressData: {[key: number]: boolean}): Array<string> {
+    var dataBlob = [];
+    for (var ii = 0; ii < 100; ii++) {
+      var pressedText = pressData[ii] ? ' (pressed)' : '';
+      dataBlob.push('Row ' + ii + pressedText);
+    }
+    return dataBlob;
   },
-  Image_outer:{
-     flex: 1,
-  }
+
+  _pressRow: function(rowID: number) {
+    this._pressData[rowID] = !this._pressData[rowID];
+    this.setState({dataSource: this.state.dataSource.cloneWithRows(
+      this._genRows(this._pressData)
+    )});
+  },
 });
+
+var THUMB_URLS = [
+  require('../image/1.jpg'),
+  require('../image/2.jpg'),
+  require('../image/3.jpg'),
+  require('../image/4.jpg'),
+  require('../image/5.jpg'),
+  require('../image/6.jpg'),
+  require('../image/7.jpg'),
+  require('../image/8.jpg'),
+  require('../image/9.jpg'),
+  require('../image/10.jpg'),
+  require('../image/11.jpg'),
+  ];
+var LOREM_IPSUM = 'Lorem ipsum dolor sit amet, ius ad pertinax oportere accommodare, an vix civibus corrumpit referrentur. Te nam case ludus inciderint, te mea facilisi adipiscing. Sea id integre luptatum. In tota sale consequuntur nec. Erat ocurreret mei ei. Eu paulo sapientem vulputate est, vel an accusam intellegam interesset. Nam eu stet pericula reprimique, ea vim illud modus, putant invidunt reprehendunt ne qui.';
+
+/* eslint no-bitwise: 0 */
+var hashCode = function(str) {
+  var hash = 15;
+  for (var ii = str.length - 1; ii >= 0; ii--) {
+    hash = ((hash << 5) - hash) + str.charCodeAt(ii);
+  }
+  return hash;
+};
+
 
 
 module.exports = Home;
